a7092d76d86b4583b78c3704b7cc80a9a6f4b67c
harioctav <aldiamahari16@gmail.com>
2024-11-01 06:42:49 +0700

    Episode 6: Parsing data to components

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-  return inertia('Questions/Index');
+  return Inertia::render('Questions/Index', [
+    'questions' => [
+      [
+        'id' => 1,
+        'title' => 'Question 1'
+      ],
+      [
+        'id' => 2,
+        'title' => 'Question 2'
+      ],
+      [
+        'id' => 3,
+        'title' => 'Question 3'
+      ]
+    ]
+  ]);
 });
