f180d6147a8033a631f814b48b4d6621ae2aa6cd
harioctav <aldiamahari16@gmail.com>
2024-11-01 13:53:40 +0700

    Episode 13: Menampilkan data pada komponen

--- routes/web.php
+++ routes/web.php
@@ -6,27 +6,11 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-
-  return QuestionResource::collection(
-    Question::with('user')->latest()->paginate(15)
-  );
-
-  // return Inertia::render('Questions/Index', [
-  //   'questions' => [
-  //     [
-  //       'id' => 1,
-  //       'title' => 'Question 1'
-  //     ],
-  //     [
-  //       'id' => 2,
-  //       'title' => 'Question 2'
-  //     ],
-  //     [
-  //       'id' => 3,
-  //       'title' => 'Question 3'
-  //     ]
-  //   ]
-  // ]);
+  return Inertia::render('Questions/Index', [
+    'questions' => QuestionResource::collection(
+      Question::with('user')->latest()->paginate(15)
+    )
+  ]);
 })->name('questions.index');
 
 Route::get('/questions/{id}', function ($id) {
