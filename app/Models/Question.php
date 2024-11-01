3012a18671cd05f11dcaf8744128552af207d271
harioctav <aldiamahari16@gmail.com>
2024-11-01 13:05:35 +0700

    Episode 10: Tabel Migrasi

---
+++ app/Models/Question.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Question extends Model
+{
+  /** @use HasFactory<\Database\Factories\QuestionFactory> */
+  use HasFactory;
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'user_id',
+    'title',
+    'slug',
+    'body',
+    'votes_count',
+    'views_count',
+    'answers_count',
+  ];
+
+  /**
+   * Get the user that owns the question.
+   *
+   * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+   */
+  public function user(): BelongsTo
+  {
+    return $this->belongsTo(User::class);
+  }
+}
